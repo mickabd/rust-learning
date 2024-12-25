aa728ba0345e5d645f352f60fc55db8d212c2603
Mickael <abdealy.mickael@gmail.com>
2024-12-25 12:52:50 +0100

    feat: add new project deck

---
+++ README.md
@@ -0,0 +1,53 @@
+# Rust Learning Repository
+
+This repository contains a Rust application to handle a deck of cards with two main functionalities: shuffle and draw.
+
+## Project Structure
+
+- `deck/`: Contains the Rust application for managing a deck of cards.
+
+## Functionalities
+
+1. **Shuffle**: Randomly shuffles the deck of cards.
+2. **Draw**: Draws a card from the deck.
+
+## Getting Started
+
+To get started with the project, follow these steps:
+
+1. Clone the repository:
+
+    ```sh
+    git clone https://github.com/yourusername/rust-learning.git
+    cd rust-learning
+    ```
+
+2. Navigate to the `deck` directory:
+
+    ```sh
+    cd deck
+    ```
+
+3. Build the project:
+
+    ```sh
+    cargo build
+    ```
+
+4. Run the application:
+
+    ```sh
+    cargo run
+    ```
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request for any changes.
+
+## Contact
+
+For any questions or feedback, please contact [abdealy.mickael@gmail.com](mailto:abdealy.mickael@gmail.com).
---
+++ deck/src/main.rs
@@ -0,0 +1,46 @@
+use rand::{seq::SliceRandom, thread_rng};
+
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        // List of 'suits' - 'hearts', 'spades'
+        // List of 'values' - 'ace', 'two', 'three'
+        // Double nested for loop
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+        Deck { cards } // implicit return
+    }
+
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
+}
+
+fn main() {
+    let mut deck = Deck::new();
+    println!("Heres your deck: {:#?}", deck);
+
+    deck.shuffle();
+
+    // Probably need to add error handling!!!
+    let cards = deck.deal(3);
+
+    println!("Heres your deck: {:#?}", cards);
+}
